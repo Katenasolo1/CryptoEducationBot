6a588cea9320d14e9a400949e04da33651b2c2c5
Katenasolo1 <51489758+Katenasolo1@users.noreply.github.com>
2022-11-28 18:17:29 +0300

    Add files via upload

---
+++ app.py.py
@@ -0,0 +1,45 @@
+import telebot
+from config import keys, TOKEN
+from extensions import APIException, CryptoConverter
+
+
+bot = telebot.TeleBot(TOKEN)
+
+# Обрабатываются все сообщения, содержащие команды '/start' or '/help'.
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message):
+    bot.send_message(message.chat.id, "Приветствую! В этом чате вы можете получить информацию по конвертации валюты. \n"
+                                      "Для этого необходимо ввести данные в формате: \n"
+                                      "<имя валюты > <в какую валюту перевести> <количество переводимой валюты> \n"
+                                      "Пример ввода данных: евро рубль 100 \n"
+                                      "Бот выдаст информацию о переводе 100 евро в рубли по текущему курсу \n"
+                                      "Доступную валюту для конвертации можно узнать с помощью '/values'")
+    pass
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = "Доступные валюты:"
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+    pass
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise APIException('Введите три параметра, где первое исходная валюта, второе валюта в которую переводим, третье количество переводимой валюты')
+
+        quote, base, amount = values
+        total_base = CryptoConverter.get_price(quote, base, amount)
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(f'Не удалось выполнить команду\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+bot.polling(none_stop=True)
---
+++ config.py
@@ -0,0 +1,5 @@
+TOKEN = "5902249368:AAGcubJfwVVDouJtVvGPs_rV0i5gAF6eG70"
+keys = {'евро': 'EUR',
+        'доллар': 'USD',
+        'рубль': 'RUB'
+}
